69ee85ab62a317a4b8872fac5ec86d15089ae2f1
xoomdev <xoomdevnet@gmail.com>
2012-12-09 09:18:59 -0600

    d2: Update to phone-xhdpi-2048-dalvik-heap.mk

--- d2-common.mk
+++ d2-common.mk
@@ -103,5 +103,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
