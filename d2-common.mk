0801d5e2b8b157ce7f1f550cb559017bde413a7a
Shareef Ali <shareefalis@cyanogenmod.org>
2013-01-06 03:57:11 -0600

    d2: adds support for 5.1 sound
    Change-Id: I5cdb6c084f8db4466f6701894a798308072a2253

--- d2-common.mk
+++ d2-common.mk
@@ -102,7 +102,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data_netmgrd_nint=16 \
     lpa.decode=true \
     rild.libpath=/system/lib/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM
+    ril.subscription.types=NV,RUIM \
+    media.aac_51_output_enabled=true
 
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
