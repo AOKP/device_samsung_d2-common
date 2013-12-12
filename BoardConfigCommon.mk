37da78f0730dfbd8f72f920a86260f4eeeb17451
Dan Pasanen <dan.pasanen@gmail.com>
2013-12-11 19:19:49 -0600

    Camera: enable TARGET_NEED_CAMERA_ZSL
    Change-Id: Ifcb18dc9cf9238aa53627edb886d8cc3f206a630

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,6 +71,9 @@ BOARD_HAVE_DOCK_USBAUDIO := true
 #camera abi compatiblily
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
+ifeq ($(filter apexqtmo expressatt,$(VARIENT_MODEL)),)
+  TARGET_NEED_CAMERA_ZSL := true
+endif
 
 # Separate audio devices for VOIP
 BOARD_USES_SEPERATED_VOIP := true
