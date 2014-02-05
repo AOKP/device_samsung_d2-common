505015b3d191196d28d88a36dd6ded4021ffeaad
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-02-05 20:48:13 +0100

    d2: Unify
    * Amended from dhacker's use for ghost and falcon
    
    d2: sed -i 's/d2-common/d2/g'
    
    Change-Id: Ic4055a291bd25a39ffcdd93b426fa30ae53827ba
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: More unification and add init_d2.c
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: Add custom releasetools for loading correct ril bits
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: set gsm.current.phone-type based on bootloader
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: declare correct char variable to be used for switch
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: Add asserts for our devices
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: TARGET_UNIFIED_DEVICE
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    init_d2: use strstr to match bootloader
    
    * And set rild.libargs once
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: set ro.telephony.default_cdma_sub for cdma variants
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: correct is_substring checks in releasetools.py
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    
    d2: relocate init_d2.c to init/
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,16 @@
 # inherit from common msm8960
 -include device/samsung/msm8960-common/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/d2-common/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/d2/include
+
+# Vendor init
+TARGET_UNIFIED_DEVICE := true
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/d2/init/init_d2.c
+
+# Allow our d2's
+TARGET_OTA_ASSERT_DEVICE := d2att,d2tmo,d2usc,d2vzw
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/d2
 
 # Kernel
 TARGET_KERNEL_SOURCE        := kernel/samsung/d2
@@ -41,16 +50,16 @@ WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
 WIFI_DRIVER_MODULE_NAME := "dhd"
 
 # Adreno configuration
-BOARD_EGL_CFG := device/samsung/d2-common/configs/egl.cfg
+BOARD_EGL_CFG := device/samsung/d2/configs/egl.cfg
 
 # Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/d2-common/recovery/recovery_keys.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/d2/recovery/recovery_keys.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := device/samsung/d2-common/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/samsung/d2/rootdir/etc/fstab.qcom
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
@@ -65,8 +74,8 @@ TARGET_USES_QCOM_BSP := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
 
 # bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2-common/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2-common/bluetooth/vnd_d2.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2/bluetooth/vnd_d2.txt
 
 # Disable initlogo, Samsungs framebuffer is weird
 TARGET_NO_INITLOGO := true
--- extract-files.sh
+++ extract-files.sh
@@ -34,7 +34,7 @@ for FILE in `egrep -v '(^#|^$)' ../$DEVICE/device-proprietary-files.txt`; do
   fi
 done
 
-for FILE in `egrep -v '(^#|^$)' ../d2-common/common-proprietary-files.txt`; do
+for FILE in `egrep -v '(^#|^$)' ../d2/proprietary-files.txt`; do
   echo "Extracting /system/$FILE ..."
   DIR=`dirname $FILE`
   if [ ! -d $BASE/$DIR ]; then
@@ -47,4 +47,4 @@ for FILE in `egrep -v '(^#|^$)' ../d2-common/common-proprietary-files.txt`; do
   fi
 done
 
-./../d2-common/setup-makefiles.sh
+./../d2/setup-makefiles.sh
